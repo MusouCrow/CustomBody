e8cb4e504d593f77da404431b3442f772fe3369e
MusouCrow <939549924@qq.com>
2023-03-24 13:19:35 +0800

    Test with velocity

--- Assets/Source/Tester.cs
+++ Assets/Source/Tester.cs
@@ -3,8 +3,7 @@ using UnityEngine;
 public class Tester : MonoBehaviour {
     public float radius = 1;
     public float height = 1.5f;
-    public float distance = 10;
-    public Vector3 direction = new Vector3(0, 0, 1);
+    public Vector3 velocity;
 
     private Vector3 target;
     private GameObject aim;
@@ -15,46 +14,39 @@ public class Tester : MonoBehaviour {
         cc.radius = radius;
         cc.height = height;
         cc.enabled = false;
-
-        this.direction = this.direction.normalized;
     }
 
     protected void Update() {
         RaycastHit hit;
+
+        var direction = this.velocity.normalized;
+        var distance = this.velocity.magnitude;
         
         var row = new Vector3(this.radius, this.height * 0.5f, this.radius);
-        row = row.Mul(this.direction);
+        row = row.Mul(direction);
 
         var position = this.transform.position - row;
         var height = Vector3.up * this.height;
         var p1 = position + height * 0.25f;
         var p2 = position - height * 0.25f;
 
-        bool ok = Physics.CapsuleCast(p1, p2, this.radius, this.direction, out hit, this.distance);
+        bool ok = Physics.CapsuleCast(p1, p2, this.radius, direction, out hit, distance);
 
         if (ok) {
             this.target = hit.point - row;
         }
         else {
-            this.target = position + this.direction * this.distance;
+            this.target = position + direction * distance;
         }
 
         this.aim.transform.position = this.target;
     }
 
     protected void OnDrawGizmos() {
-        /*
-        var matrix = Matrix4x4.identity;
-        matrix.SetTRS(Vector3.zero, Quaternion.identity, new Vector3(this.radius, this.height, this.radius));
-        Gizmos.matrix = matrix;
-        */
-        // Gizmos.color = Color.green;
-        // Gizmos.DrawWireSphere(this.transform.position, 1);
+        Gizmos.color = Color.white;
+        Gizmos.DrawLine(this.transform.position, this.target);
 
         Gizmos.color = Color.red;
         Gizmos.DrawSphere(this.target, 0.1f);
-        // Gizmos.DrawWireSphere(this.target, 1);
-
-        // Gizmos.matrix = Matrix4x4.identity;
     }
 }
