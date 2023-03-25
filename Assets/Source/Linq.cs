4b1c019f06731a18a3aa64bab7ed6f0f3697cec5
MusouCrow <939549924@qq.com>
2023-03-26 01:51:49 +0800

    Rule

--- Assets/Source/Linq.cs
+++ Assets/Source/Linq.cs
@@ -4,4 +4,8 @@ public static class Linq {
     public static Vector3 Mul(this Vector3 a, Vector3 b) {
         return new Vector3(a.x * b.x, a.y * b.y, a.z * b.z);
     }
+
+    public static bool Equal(this float a, float b) {
+        return Mathf.Approximately(a, b);
+    }
 }
--- Assets/Source/Tester.cs
+++ Assets/Source/Tester.cs
@@ -1,58 +1,131 @@
 using UnityEngine;
 
 public class Tester : MonoBehaviour {
+    public struct CastHit {
+        public bool collided;
+        public Vector3 point;
+        public Vector3 normal;
+        public Vector3 direction;
+        public float distance;
+        public GameObject gameObject;
+    }
+
     public float radius = 1;
     public float height = 1.5f;
     public Vector3 velocity;
 
-    private Vector3 target;
-    private Vector3 normal;
-    private GameObject aim;
+    private CastHit groundHit;
+    private CastHit moveHit;
 
-    protected void Awake() {
-        this.aim = new GameObject("Aim");
-        var cc = this.aim.AddComponent<CharacterController>();
-        cc.radius = radius;
-        cc.height = height;
-        cc.enabled = false;
+    public bool InGround {
+        get {
+            return this.groundHit.distance <= 0.1f;
+        }
     }
 
-    protected void Update() {
-        RaycastHit hit;
+    protected void Update() {        
+        this.CheckGround();
 
         var direction = this.velocity.normalized;
         var distance = this.velocity.magnitude;
-        
-        var row = new Vector3(this.radius, this.height * 0.5f, this.radius);
-        row = row.Mul(direction);
 
-        var position = this.transform.position - row;
-        var height = Vector3.up * this.height;
-        var p1 = position + height * 0.25f;
-        var p2 = position - height * 0.25f;
-
-        bool ok = Physics.CapsuleCast(p1, p2, this.radius, direction, out hit, distance);
-
-        if (ok) {
-            this.target = position + direction * hit.distance;
-            this.normal = hit.normal;
+        if (this.velocity.y.Equal(0) && this.InGround) {
+            this.moveHit = this.SimulateLand(this.transform.position, direction, distance);
         }
         else {
-            this.target = position + direction * distance;
-            this.normal = Vector3.zero;
+            this.moveHit = this.SimulateFree(this.transform.position, direction, distance);
         }
-
-        this.aim.transform.position = this.target;
     }
 
     protected void OnDrawGizmos() {
+        if (!Application.isPlaying) {
+            return;
+        }
+
+        this.DrawCapsuleGizmos(this.transform.position, Color.yellow);
+        this.DrawCapsuleGizmos(this.moveHit.point, Color.green);
+
+        if (!this.InGround) {
+            this.DrawCapsuleGizmos(this.groundHit.point, Color.black);
+        }
+        else {
+            Gizmos.color = Color.red;
+            var normal = Vector3.ProjectOnPlane(this.transform.forward, this.groundHit.normal);
+            Gizmos.DrawRay(this.groundHit.point, normal);
+        }
+
+        if (this.groundHit.gameObject) {
+            var go = this.groundHit.gameObject;
+            var t = go.transform;
+            var filter = go.GetComponent<MeshFilter>();
+
+            if (filter) {
+                Gizmos.color = new Color(1, 0, 0, 0.5f);
+                Gizmos.DrawMesh(filter.sharedMesh, 0, t.position, t.rotation, t.lossyScale);
+            }
+        }
+
         Gizmos.color = Color.white;
-        Gizmos.DrawLine(this.transform.position, this.target);
+        Gizmos.DrawLine(this.transform.position, this.moveHit.point);
+    }
 
-        Gizmos.color = Color.green;
-        Gizmos.DrawRay(this.target, this.normal);
+    private void DrawCapsuleGizmos(Vector3 position, Color color) {
+        var radius = this.radius * Vector3.up;
+        var height = this.height * Vector3.up;
 
-        Gizmos.color = Color.red;
-        Gizmos.DrawSphere(this.target, 0.1f);
+        Gizmos.color = color;
+        Gizmos.DrawSphere(position, 0.1f);
+
+        Gizmos.DrawWireSphere(position + radius, this.radius);
+        Gizmos.DrawWireSphere(position + height + radius, this.radius);
+    }
+
+    private void CheckGround() {
+        this.groundHit = this.CapsuleCast(this.transform.position, Vector3.down, 100);
+    }
+
+    private CastHit SimulateFree(Vector3 position, Vector3 direction, float distance) {
+        var hit = this.CapsuleCast(position, direction, distance);
+        
+        return hit;
+    }
+
+    private CastHit SimulateLand(Vector3 position, Vector3 direction, float distance) {
+        var hit = this.CapsuleCast(position, direction, distance);
+        
+        return hit;
+    }
+
+    private CastHit CapsuleCast(Vector3 position, Vector3 direction, float distance) {
+        var shift = new Vector3(this.radius, this.height, this.radius);
+        shift = shift.Mul(direction);
+        
+        var height = Vector3.up * this.height;
+        var radius = Vector3.up * this.radius;
+
+        var p1 = position + radius;
+        var p2 = position + radius + height;
+        RaycastHit hit;
+        bool collided = Physics.CapsuleCast(p1, p2, this.radius, direction, out hit, distance);
+        
+        Vector3 normal = Vector3.zero;
+        GameObject gameObject = null;
+
+        if (collided) {
+            distance = hit.distance;
+            normal = hit.normal;
+            gameObject = hit.collider.gameObject;
+        }
+
+        Vector3 point = position + direction * distance;
+
+        return new CastHit() {
+            collided = collided,
+            point = point,
+            normal = normal,
+            direction = direction,
+            distance = distance,
+            gameObject = gameObject
+        };
     }
 }
